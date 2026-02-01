906153bcaa2250511ad6b783e343420adb0e8356
Rohan0409 <rohantalware123@gmail.com>
2026-02-02 00:42:41 +0530

    Update App.jsx for HashRouter and demo init

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 
 
 
